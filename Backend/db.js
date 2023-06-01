7c58f6c9219b6ece80a2295fc3d74036a60d11fe
Ankita <ankitaroy0807@gmail.com>
2023-06-01 21:44:21 +0530

    initial

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI ="mongodb+srv://saumyashipra:shipra%4012@cluster0.kf0p4vu.mongodb.net/user"
+const mongoURI ="mongodb+srv://ankitaroy0807:Ankita%40-%2B@cluster0.c9edoze.mongodb.net/customers"
 
 //connecting to mongoose
 const connectToMongo = ()=>{
